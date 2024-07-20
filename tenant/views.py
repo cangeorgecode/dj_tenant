d2a222fe4eff99acd78242df0c5b5beed08352a5
root <root@MSI>
2024-07-20 09:45:58 +0800

    init

---
+++ tenant/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Member
+from .utilities import get_tenant
+
+def our_team(request):
+    tenant = get_tenant(request)
+    members = Member.objects.filter(tenant=tenant)
+
+    return render(request, 'tenant/our_team.html', {'tenant': tenant, 'members': members})
